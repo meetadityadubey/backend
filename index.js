6bdd6b10dbbdca97af50477571fffd08dc4d3346
aditya <fordevelopmentusecase@gmail.com>
2025-07-18 13:07:46 +0530

    added fallback port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;  //fallback to 3000 in case port is not defined
 
 const githubData = {
   "login": "hiteshchoudhary",
